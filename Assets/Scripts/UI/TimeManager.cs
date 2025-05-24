67d963ed564e03b81b692a8575e880920087994d
Rex0Liang <rex.liang@education.nsw.gov.au>
2025-05-24 16:45:34 +1000

    Added Time System

---
+++ Assets/Scripts/UI/TimeManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] public float currentTime = 60f;
+    private bool active = true;
+
+    private void Update()
+    {
+        if (!active)
+            return;
+
+        currentTime -= Time.deltaTime;
+
+        UpdateTimerUI();
+
+        if (currentTime <= 0)
+        {
+            StopTimer();
+        }
+    }
+
+    public void StopTimer()
+    {
+        active = false;
+        currentTime = 0f;
+        UpdateTimerUI();
+    }
+
+    private void UpdateTimerUI()
+    {
+        if (currentTime > 0 && currentTime < 6)
+        {
+            timerText.color = Color.yellow;
+        }
+        else if (currentTime < 1)
+        {
+            timerText.color = Color.red;
+        }
+
+        TimeSpan t = TimeSpan.FromSeconds(currentTime);
+        timerText.text = t.ToString(@"mm\:ss");
+    }
+}
