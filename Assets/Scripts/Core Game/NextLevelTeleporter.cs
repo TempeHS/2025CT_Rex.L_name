ca50e13433ba72ead94cc228e93378130bc51051
Rex0Liang <rex.liang@education.nsw.gov.au>
2025-05-25 12:13:49 +1000

    Lighting and Level Display

---
+++ Assets/Scripts/Core Game/NextLevelTeleporter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelTeleporter : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+    
+    public LevelDisplay levelDisplay;
+
+    void Start()
+    {
+        levelDisplay.setLevelText();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GoToScene(sceneName);
+        }
+    }
+
+    public void GoToScene(string sceneName)
+    {
+        levelDisplay.levelNumber++;
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/Scripts/UI/LevelDisplay.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LevelDisplay : MonoBehaviour
+{
+    public TextMeshProUGUI levelDisplay;
+    public NextLevelTeleporter nextLevelTeleporter;
+    public int levelNumber = 1;
+
+    public void Update()
+    {
+        setLevelText();
+    }
+    public void setLevelText()
+    {
+        levelDisplay.text = "Level " + levelNumber.ToString();
+    }
+}
