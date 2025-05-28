baf0e4e59608c117cc49e60a06c0bc50e1e729ab
Rex0Liang <rex.liang@education.nsw.gov.au>
2025-05-28 17:06:04 +1000

    Fixed UI and added dashing

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,30 +4,78 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //Start movement variables
-    private float horizontal; 
-    [SerializeField] private float speed = 5f;
-    [SerializeField] private float jumpingPower = 15f;
+    [SerializeField] private Rigidbody2D rb;
+    private TrailRenderer trailRenderer;
+    private Animator animator;
+
+    [Header("Ground Check Variables")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
     private bool isFacingRight = true;
 
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer; 
-    
-    //End movement variables
+
+    [Header("Movement Variables")]
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpingPower = 15f;
+    private float horizontal; 
+
+
+    [Header("Dashing Variables")]
+    [SerializeField] private float dashingVelocity = 18f;
+    [SerializeField] private float dashingTime = 0.75f;
+    private Vector2 dashingDirection;
+    private bool isDashing;
+    private bool canDash = true;
+    private bool dashInput;
+
+    void Start()
+    {
+        trailRenderer = GetComponent<TrailRenderer>();
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+        dashInput = Input.GetButtonDown("Dash");
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
 
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f) // Jump input
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f) 
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        if (dashInput && canDash)
+        {
+            isDashing = true;
+            canDash = false;
+            trailRenderer.emitting = true;
+            dashingDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+            /* if (dashingDirection == Vector2.zero)
+            {
+                dashingDirection = new Vector2(transform.localScale.x, );
+            }
+            */
+
+            StartCoroutine(StopDashing());
+        }
+
+        animator.SetBool("IsDashing", isDashing);
+
+        if (isDashing)
+        {
+            rb.velocity = dashingDirection.normalized * dashingVelocity;
+            return;
+        }
+
+        if (IsGrounded())
+        {
+            canDash = true;
         }
 
         Flip();
@@ -40,13 +88,21 @@ public class PlayerMovement : MonoBehaviour
     }
 
 
+    private IEnumerator StopDashing()
+    {
+        yield return new WaitForSeconds(dashingTime);
+        trailRenderer.emitting = false;
+        isDashing = false;
+    }
+
+
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
 
-    private void Flip() // Flip player face function
+    private void Flip()
     {
         if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
